cb540a50b87aad24f1d6737ea519c98e37969853
I Love Iced Coffee <ashtonshyti@outlook.com>
2024-02-15 18:03:49 -0500

    Make sure you reorganized the files when you make new repository
    Assets, Library, etc folders goes into the folder with .gitignore & .gitattributes.

---
+++ Assets/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour
+{
+    [Header("Cube Object")]
+    public GameObject currentCube;
+    [Header("Last Cube Object")]
+    public GameObject lastCube;
+    [Header("Text Object")]
+    public Text text;
+    [Header("Current Level")]
+    public int Level;
+    [Header("Boolean")]
+    public bool Done;
+
+    void NewBlock()
+    {
+        if(lastCube != null)
+        {
+            //Uncomment these when you come back Adam.
+            //Comment unfinished codes before you close Unity and/or push to GitHub
+
+            //currentCube.transform.position = new Vector3(Mathf.Round(currrentCube.transform.position.x),
+                //current
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
