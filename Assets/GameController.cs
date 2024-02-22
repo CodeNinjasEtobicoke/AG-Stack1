9ddf787310448b54a447e8ccdbbc1be3f7ffaf4a
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-02-22 17:56:19 -0500

    did stuff

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -20,23 +20,54 @@ public class GameController : MonoBehaviour
     {
         if(lastCube != null)
         {
-            //Uncomment these when you come back Adam.
-            //Comment unfinished codes before you close Unity and/or push to GitHub
-
-            //currentCube.transform.position = new Vector3(Mathf.Round(currrentCube.transform.position.x),
-                //current
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+                currentCube.transform.position.y,
+                Mathf.Round(currentCube.transform.position.z));
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                            lastCube.transform.localScale.y,
+                                                            lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+            if (currentCube.transform.localScale.x<= 0f ||
+                currentCube.transform.localScale.z <= 0f)
+            {
+                Done = true;
+                text.gameObject.SetActive(true);
+                text.text = "Final Score: " + Level;
+                StartCoroutine(X());
+                return;
+            }
         }
+
+        lastCube = currentCube;
+        currentCube = Instantiate(lastCube);
+        currentCube.name = Level + "";
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_color", Color.HSVToRGB((Level / 100f) % 1f, 1f,1f));
+        Level++;
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        NewBlock();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Done)
+        {
+            return;
+        }
+        var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
+        var pos1 = lastCube.transform.position + Vector3.up * 10f;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        if (Level % 2 == 0)
+        {
+            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+        }
+        else
+        { 
     }
 }
