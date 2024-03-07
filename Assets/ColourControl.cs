2b153f36551d46e2c4ba425fb0d564180b2f2916
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-03-07 17:25:13 -0500

    Finished Prove yourself

---
+++ Assets/ColourControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourControl : MonoBehaviour
+{
+    private Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        ChangeColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void ChangeColor()
+    {
+        mat.color = new Color(0f, 0f, 0f);
+    }
+}
